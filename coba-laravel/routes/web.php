178481bc2e590ee21a3bac8868f4ae5bf8ce0a25
rifky86 <goblogpayah123@gmail.com>
2021-10-11 00:58:05 +0700

    Struktur Folder,Routes & View (tugas5)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rifky Maulana",
+        "email" => "193040068@mail.unpas.ac.id",
+        "img" => "IMG_wtzmr6.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
